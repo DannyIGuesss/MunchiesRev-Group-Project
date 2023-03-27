46486422e47dc8ac4286e51d9451590bb582cb50
DannyIGuesss <danielgarcia448353@gmail.com>
2023-03-27 13:23:43 -0700

    I worked on the landing page, adding images and changing the colors

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom'
 import ('../cssFiles/navBar.css')
+import('../cssFiles/landing.css')
 
 const Landing = (props) => {
+
     return (
         <div>
             <nav>
@@ -12,6 +14,17 @@ const Landing = (props) => {
                     <button><Link to={'/Login'}>Login</Link></button>
                 </div>
             </nav>
+            <div className='main-body'>
+                <div className='left-side'>
+                    <h2>What we do here at MunchiesRev</h2>
+                    <p>Someone who is more descriptive can write about what the website is about!</p>
+                </div>
+                <div className='right-side'>
+                    <div className='slider'>
+                        {/* this div is for the images slider */}
+                    </div>
+                </div>
+            </div>
         </div>
 )}
 
